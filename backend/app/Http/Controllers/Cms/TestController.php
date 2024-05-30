6f5895c0d144b105daea61103b0bcc6ff42646ae
M-IhsanMaulana <muhammadihsanm05@gmail.com>
2024-05-30 09:33:12 +0700

    feat: Install Api & Setup Structure

---
+++ backend/app/Http/Controllers/Cms/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use App\Http\Controllers\CmsController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends CmsController
+{
+    //
+}
